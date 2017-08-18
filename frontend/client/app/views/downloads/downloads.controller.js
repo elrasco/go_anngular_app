ee3c58eee8fbb8789c34ea76215fbc5b251be32a
luca@mint <luca.rasconi@gmail.com>
2017-08-18 17:43:10 +0200

    added api key

--- frontend/client/app/views/downloads/downloads.controller.js
+++ frontend/client/app/views/downloads/downloads.controller.js
@@ -5,6 +5,8 @@ class DownloadsController {
 
     this.$mdSidenav = $mdSidenav;
 
+    this.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyCgUcK5V8fDzPEzwUynWQ2kZe7UDvEmXVo";
+
     NgMap.getMap().then(map => {
       //I have to resize: https://github.com/allenhwkim/angularjs-google-maps#grey-area-in-google-maps
       let center = map.getCenter();
